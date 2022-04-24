2a6a119b1be3942efe064710c02c627e3e5bc498
isaac mariga <isaacwangombe5@gmail.com>
2022-04-24 19:49:54 +0300

    Function to create credential

--- run.py
+++ run.py
@@ -2,9 +2,17 @@
 
 from password import Credentials
 
+
 def create_credential(account, password):
     '''
     Function to create a new credential
     '''
-    new_credential = Credentials(account,password)
+    new_credential = Credentials(account, password)
     return new_credential
+
+
+def save_credentials(credential):
+    '''
+    Function to save credential
+    '''
+    credential.save_credential()
