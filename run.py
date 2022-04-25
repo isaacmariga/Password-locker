adc5a8ecf20302fd45fb28b0b342df5694bd8fe2
isaac mariga <isaacwangombe5@gmail.com>
2022-04-25 05:50:34 +0300

    entering logic for logging in

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3.9
 
 from password import Credentials
+from password import User
 
+def create_user(owner, key):
+    '''
+    function to create a new user
+    '''
+    new_user = User(owner, key)
+    return new_user
+
+def save_user(data):
+    '''
+    function to save a new user
+    '''
+    data.save_user()
 
 def create_credential(account, password):
     '''
@@ -54,58 +67,77 @@ def copy_password():
 
 
 def main():
-    print("Hello Welcome to your Password-locker. What is your name?")
-    user_name = input()
+    print("Hello Welcome to your Password-locker. What would you like to do?")
+    
+    print("\n 1. Create an account \n 2 Exit")
+    option = input()
 
-    print(f"Hello {user_name}. what would you like to do?")
-    print('\n')
 
-    while True:
-        print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the password-locker")
-        short_code = input().lower()
-        if short_code == 'cc':
-            print("New Credential")
-            print("-"*10)
-            print("Account ....")
-            account = input()
-            print("Password...")
-            passsword = input()
-
-            # create and save new credential.
-            save_credentials(create_credential(account, passsword))
-            print('\n')
-            print(f"New Credential {account} {passsword} created")
-            print('\n')
-        elif short_code == 'dc':
-            if display_credentials():
-                print("Here is a list of all your credentials")
-
-                print('\n')
-
-                for credential in display_credentials():
-
-                    print(f"Account: {credential.account} Password:{credential.password}")
-                print('\n')
+    if option == "1":
+            print("what is your name?")
+            owner = input()
+            if not owner:
+                print("You must type in something")
             else:
-                print('\n')
-                print("You dont seem to have any credentials saved yet")
-                print('\n')
-        elif short_code == 'fc':
-            print("Enter the account you want to search for")
+                print("Enter password")
+                key = input()
+                if not owner:
+                        print("You must type in something")
+                else:
+                        save_user(create_user(owner, key))
+                        print("-"*10)
+                        print(f"{owner} please enter your password again to enter")
+                        new_key = input()
+                        if new_key == key:
+                                print("/n You have successfully logged in")
 
-            search_account = input()
 
-            if check_existing_credentials(search_account):
-                search_credential = find_credential(search_account)
-                print(f"Your {search_credential.account} Password is: {search_credential.password}")
-                print('-' * 20)
-            else:
-                print("That credential does not exist")
-        elif short_code == "ex":
-            print("Bye .......")
-            break
-        else:
-            print("I really didn't get that. Please use the short codes")
+                        while True:
+                            print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the password-locker")
+                            short_code = input().lower()
+                            if short_code == 'cc':
+                                print("New Credential")
+                                print("-"*10)
+                                print("Account ....")
+                                account = input()
+                                print("Password...")
+                                passsword = input()
+
+                                # create and save new credential.
+                                save_credentials(create_credential(account, passsword))
+                                print('\n')
+                                print(f"New Credential {account} {passsword} created")
+                                print('\n')
+                            elif short_code == 'dc':
+                                if display_credentials():
+                                    print("Here is a list of all your credentials")
+
+                                    print('\n')
+
+                                    for credential in display_credentials():
+                                    
+                                        print(f"Account: {credential.account} Password:{credential.password}")
+                                    print('\n')
+                                else:
+                                    print('\n')
+                                    print("You dont seem to have any credentials saved yet")
+                                    print('\n')
+                            elif short_code == 'fc':
+                                print("Enter the account you want to search for")
+
+                                search_account = input()
+
+                                if check_existing_credentials(search_account):
+                                    search_credential = find_credential(search_account)
+                                    print(f"Your {search_credential.account} Password is: {search_credential.password}")
+                                    print('-' * 20)
+                                else:
+                                    print("That credential does not exist")
+                            elif short_code == "ex":
+                                print("Bye .......")
+                                break
+                            else:
+                                print("I really didn't get that. Please use the short codes")
 
 
 if __name__ == '__main__':
